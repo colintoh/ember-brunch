78766d269af4f2492b0922a2049f85102f96f785
Colin Toh <colinnus@gmail.com>
2013-07-25 14:03:32 +0800

    Remove placeholder stuff

--- app/template.js
+++ app/template.js
@@ -1,7 +1,3 @@
 require('templates/application');
 require('templates/home');
 require('templates/nextpage');
-require('templates/mary');
-require('templates/recipe');
-require('templates/attraction');
-require('templates/oliya');
