3be2538a5b065ba371a61d3794a052e370513a29
Colin Toh <colinnus@gmail.com>
2014-07-23 01:08:25 +0800

    add compression for express server

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ if(cluster.isMaster){
     }
 } else {
 	var express = require('express'),
+		compression = require('compression'),
 		app = express(),
 		port = process.env.PORT || 8080;
 
+	app.use(compression());
 	app.use(express.static(__dirname + '/public'));
 
 	exports.startServer = function(port,path,callback){
@@ -21,6 +23,8 @@ if(cluster.isMaster){
 		app.listen(port);
 		console.log("Server started on port " + port);
 	}
+
+	exports.startServer(3000);
 
 	if( process.env.PORT ){
 		exports.startServer(process.env.PORT);
