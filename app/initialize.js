8d7da7831a4fefd86245db46335ea4f0f99fbc75
Lam Tuan <nlt2390@yahoo.com>
2014-07-22 14:06:54 +0700

    inittialize.js with order

--- app/initialize.js
+++ app/initialize.js
@@ -1,20 +1,35 @@
 window.App = require('app');
 
+var regString,
+		excludeString,
+		fileList = window.require.list();
 
-// Require All Template files
-window.require.list().filter(function(module){
-	return new RegExp('^templates/').test(module);
-}).forEach(function(module){
-	require(module);
-});
+var requireOrder = {
+	helpers: [
+		'home' //Example: helpers/include home.js first
+	],
+	templates: [
+	]
+}
 
+var folderOrder = ['helpers', 'templates'];
 
-// Require All Helper files
-window.require.list().filter(function(module){
-	return new RegExp('^helpers/').test(module);
-}).forEach(function(module){
-	require(module);
-});
+folderOrder.forEach(function(folder){
+	
+	//Require before
+	requireOrder[folder].forEach(function(module){
+		require( folder + '/' + module);
+	});
 
+	//Require after
+	excludeString = requireOrder[folder].join('$|');
+	regString = '^'+folder+'/(?!' + excludeString + '$)';
+	fileList.filter(function(module){
+		return new RegExp(regString).test(module);
+	}).forEach(function(module){	
+		require(module);
+	});
+
+})
 
 require('router');
