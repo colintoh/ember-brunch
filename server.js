fdb0107da142be236a5c90040d7aa165d86a0ce1
Colin Toh <colinnus@gmail.com>
2014-07-23 01:13:46 +0800

    add comments

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-var
-	cluster = require('cluster'),
+var cluster = require('cluster'),
 	numCPUs = require('os').cpus().length;
 
+//If it's still in master thread, divide them into separate work thread
 if(cluster.isMaster){
+
 	for (var i = 0; i < numCPUs; i += 1) {
         cluster.fork();
     }
+
 } else {
 	var express = require('express'),
 		compression = require('compression'),
 		app = express(),
 		port = process.env.PORT || 8080;
 
-	app.use(compression());
+	app.use(compression()); //cut the fats!
 	app.use(express.static(__dirname + '/public'));
 
 	exports.startServer = function(port,path,callback){
@@ -23,8 +25,6 @@ if(cluster.isMaster){
 		app.listen(port);
 		console.log("Server started on port " + port);
 	}
-
-	exports.startServer(3000);
 
 	if( process.env.PORT ){
 		exports.startServer(process.env.PORT);
